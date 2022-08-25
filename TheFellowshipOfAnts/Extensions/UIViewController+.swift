e0639d883e69f4bce8f3d6130b835e3d22068622
SeYeongYoon <ssyoun4092@naver.com>
2022-08-25 15:36:06 +0900

    Create UIViewController+.swift

---
+++ TheFellowshipOfAnts/Extensions/UIViewController+.swift
@@ -0,0 +1,22 @@
+import Foundation
+import UIKit
+import SwiftUI
+
+#if DEBUG
+extension UIViewController {
+    private struct UIVieweControllerPreview: UIViewControllerRepresentable {
+        let viewController: UIViewController
+
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+
+    func toPreview() -> some View {
+        UIVieweControllerPreview(viewController: self)
+    }
+}
+#endif
