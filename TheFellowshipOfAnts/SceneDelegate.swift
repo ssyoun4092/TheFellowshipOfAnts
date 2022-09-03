15473aa86618a8e98343a47d887940f2294d7151
SeYeongYoon <ssyoun4092@naver.com>
2022-09-03 21:00:20 +0900

    [추가] Auth 관련 ViewController Coordinator 패턴 적용

--- TheFellowshipOfAnts/SceneDelegate.swift
+++ TheFellowshipOfAnts/SceneDelegate.swift
@@ -4,6 +4,7 @@ import KakaoSDKAuth
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var appCoordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         if let url = URLContexts.first?.url {
@@ -16,10 +17,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
+
         window = UIWindow(windowScene: windowScene)
-        window?.backgroundColor = .systemBackground
-        window?.rootViewController = UINavigationController(rootViewController: LoginViewController())
-        window?.makeKeyAndVisible()
+        self.appCoordinator = AppCoordinator(window: window!)
+        appCoordinator?.start()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
