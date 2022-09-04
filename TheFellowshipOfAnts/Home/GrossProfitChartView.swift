81d5c816324aa981310bd6d16f19be060e82f1a6
SeYeongYoon <ssyoun4092@naver.com>
2022-09-04 23:15:54 +0900

    [추가] 순이익 그래프 UI 구현

---
+++ TheFellowshipOfAnts/Home/GrossProfitChartView.swift
@@ -0,0 +1,87 @@
+//
+//  GrossProfitChartView.swift
+//  TheFellowshipOfAnts
+//
+//  Created by SeYeong on 2022/09/04.
+//
+
+import UIKit
+import Charts
+import SnapKit
+
+class GrossProfitChartView: UIView {
+
+    // MARK: - Properties
+
+    // MARK: - IBOutlets
+
+    let barChartView = BarChartView()
+
+    // MARK: - Life Cycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupBarChartView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure() {
+        let annual: [String] = ["18", "19", "20", "21"]
+        let revenue: [String] = ["53.2", "53.8", "59.6", "81.4"]
+
+        var entries = [BarChartDataEntry]()
+
+        for (index, value) in revenue.enumerated() {
+            let entryData = BarChartDataEntry(x: Double(index), y: Double(value)!)
+            entries.append(entryData)
+        }
+
+        let chartDataSet = BarChartDataSet(entries: entries)
+        chartDataSet.colors = [.red]
+        chartDataSet.valueFont = .systemFont(ofSize: 13, weight: .bold)
+        chartDataSet.valueFormatter = self
+        for (index, entry) in chartDataSet.enumerated() {
+            stringForValue(Double(revenue[index])!, entry: entry, dataSetIndex: index, viewPortHandler: nil)
+        }
+
+        barChartView.xAxis.setLabelCount(annual.count, force: false)
+        barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: annual)
+
+
+        let data = BarChartData(dataSet: chartDataSet)
+        barChartView.data = data
+    }
+}
+
+extension GrossProfitChartView {
+    private func setupBarChartView() {
+        addSubview(barChartView)
+
+        barChartView.xAxis.axisLineColor = .clear
+        barChartView.xAxis.gridColor = .clear
+        barChartView.xAxis.labelFont = .systemFont(ofSize: 15, weight: .bold)
+        barChartView.xAxis.labelPosition = .bottom
+        barChartView.leftAxis.axisMinimum = 0
+        barChartView.leftAxis.enabled = false
+        barChartView.rightAxis.enabled = false
+        barChartView.legend.enabled = false
+        barChartView.drawGridBackgroundEnabled = false
+        barChartView.drawBordersEnabled = false
+        barChartView.isUserInteractionEnabled = false
+
+        barChartView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+extension GrossProfitChartView: IValueFormatter {
+    func stringForValue(_ value: Double, entry: ChartDataEntry, dataSetIndex: Int, viewPortHandler: ViewPortHandler?) -> String {
+
+        return String(value) + "B"
+    }
+}
