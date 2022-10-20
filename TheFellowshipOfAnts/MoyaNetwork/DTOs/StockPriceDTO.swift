224b5c09e4227992bed693fc9b704e9ce40bc404
SeYeongYoon <ssyoun4092@naver.com>
2022-10-20 17:05:22 +0900

    [추가] StockPriceDTO 추가

---
+++ TheFellowshipOfAnts/MoyaNetwork/DTOs/StockPriceDTO.swift
@@ -0,0 +1,48 @@
+//
+//  StockPrice.swift
+//  TheFellowshipOfAnts
+//
+//  Created by SeYeong on 2022/10/20.
+//
+
+import Foundation
+
+extension DTO {
+    struct StockPrice: Decodable {
+        let basic: StockPriceBasic
+        let details: [StockPriceDetail]
+        let status: String
+
+        enum CodingKeys: String, CodingKey {
+            case basic = "meta"
+            case details = "values"
+            case status
+        }
+
+        struct StockPriceBasic: Decodable {
+            let symbol: String
+            let interval: String
+            let currency: String
+            let exchageTimezone: String
+            let exchange: String
+            let micCode: String
+            let type: String
+
+            enum CodingKeys: String, CodingKey {
+                case symbol, interval, currency, exchange, type
+                case exchageTimezone = "exchange_timezone"
+                case micCode = "mic_code"
+            }
+        }
+
+        struct StockPriceDetail: Decodable {
+            let dateTime, open, high, low: String
+            let close, volume: String
+
+            enum CodingKeys: String, CodingKey {
+                case dateTime = "datetime"
+                case open, high, low, close, volume
+            }
+        }
+    }
+}
