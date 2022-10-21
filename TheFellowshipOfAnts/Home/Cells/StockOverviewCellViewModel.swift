283fbd6449da0767a30da997f1e0fbac4d0c00c1
SeYeongYoon <ssyoun4092@naver.com>
2022-10-21 11:23:13 +0900

    [추가] StockIncomeStatementAPI 추가

---
+++ TheFellowshipOfAnts/Home/Cells/StockOverviewCellViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  StockOverviewCellViewModel.swift
+//  TheFellowshipOfAnts
+//
+//  Created by SeYeong on 2022/10/20.
+//
+
+import Foundation
+
+class StockOverviewCellViewModel {
+    let title: String
+    let value: String
+
+    init(title: String, value: String) {
+        self.title = title
+        self.value = value
+    }
+}
